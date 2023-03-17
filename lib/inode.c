26da298966f2fc94ad0c74be9a86cbbbb22a5141
sekaiacg <sekaiacg@gmail.com>
2023-03-17 14:09:41 +0800

    erofs-utils: mkfs: Skip `.DS_Store`
    Signed-off-by: sekaiacg <sekaiacg@gmail.com>

--- lib/inode.c
+++ lib/inode.c
@@ -1632,7 +1632,12 @@ static int erofs_mkfs_handle_directory(struct erofs_importer *im, struct erofs_i
 			goto err_closedir;
 		}
 
+#ifndef __APPLE__
 		if (is_dot_dotdot(dp->d_name)) {
+#else
+		if (is_dot_dotdot(dp->d_name) ||
+			!strncmp(dp->d_name, ".DS_Store", 9)) {
+#endif
 			++i_nlink;
 			continue;
 		}
